f6ab44c0f389a06f050f098b98ae414659db4613
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-04-01 17:49:26 -0600

    Added more bar plots, two time series, and a scatter plot

--- EAs_tyler_data.R
+++ EAs_tyler_data.R
@@ -33,6 +33,11 @@ ea_list <- unique(pals_ea$`PROJECT NUMBER`)
 proj_docs_ea <- proj_docs %>%
   filter(Project_Num %in% ea_list)
 
+length(unique(proj_docs_ea$Project_Num))
+
+# We have 1563 NEPA projects from PALS that also have documents associated with them. 
+# Print out the distribution by region, litigated, and appealed. 
+
 # First need to do a little regex to create a Project_num column in proj_overview.
 # Should be all the numbers after the 'project=' character string in the Page column
 
@@ -47,6 +52,7 @@ proj_overview_ea <- proj_overview %>%
 # Average number of documents per project?
 # Average number of documents per stage of the process (scoping, supporting, analysis, etc.)
 # Number of documents by region, appeals, litigation, etc?
+# Number of documents through time?
 # Relationship between number of documents and elapsed days?
 
 # Want an aggregated data frame with project_num in one column total number of documents in the second column.
@@ -55,6 +61,7 @@ proj_overview_ea <- proj_overview %>%
 proj_doc_count <- proj_docs_ea %>%
   count(Project_Num)
 mean(proj_doc_count$n)
+median(proj_doc_count$n)
 
 ggplot(proj_doc_count, aes(x=n)) + 
   geom_histogram(binwidth = 10) + 
@@ -69,7 +76,8 @@ proj_doc_count_stage <- proj_docs_ea %>%
   count(Project_Num, Stage)
 aggregate(proj_doc_count_stage$n, list(proj_doc_count_stage$Stage), FUN=sum)
 
-test_df <- aggregate(proj_doc_count_stage$n, list(proj_doc_count_stage$Stage), FUN=mean)
+agg_df_mean <- aggregate(proj_doc_count_stage$n, list(proj_doc_count_stage$Stage), FUN=mean)
+agg_df_median <- aggregate(proj_doc_count_stage$n, list(proj_doc_count_stage$Stage), FUN=median)
 
 proj_doc_count_stage %>% 
   ggplot(aes(x=Stage, y=n)) +
@@ -77,10 +85,101 @@ proj_doc_count_stage %>%
   ggtitle("Number of Documents by NEPA Stage") + 
   xlab("NEPA Stage") + ylab("Count")
 
-test_df %>% 
+agg_df_mean %>% 
   ggplot(aes(x=Group.1, y=x)) +
   geom_bar(stat="identity") + 
   ggtitle("Average Number of Documents per Project by NEPA Stage") + 
-  xlab("NEPA Stage") + ylab("Ave. Number of Documents by Stage per Project") # Make sure that is actually what you are averaging.
+  xlab("NEPA Stage") + ylab("Ave. Number of Documents by Stage") # Make sure that is actually what you are averaging.
 
+agg_df_median %>% 
+  ggplot(aes(x=Group.1, y=x)) +
+  geom_bar(stat="identity") + 
+  ggtitle("Average Number of Documents per Project by NEPA Stage") + 
+  xlab("NEPA Stage") + ylab("Median Number of Documents by Stage per Project")
+
+# Ave. number of documents by region, lititgation, appeals
+# Need to join the proj_docs_ea to the pals data by project number
+
+pals_doc_ea <- left_join(proj_docs_ea, pals_ea, by=c('Project_Num'='PROJECT NUMBER'))
+
+proj_doc_count_region <- pals_doc_ea %>%
+  count(Project_Num, REGION_ID)
+
+aggregate(proj_doc_count_region$n, list(proj_doc_count_region$REGION_ID), FUN=mean)
+aggregate(proj_doc_count_region$n, list(proj_doc_count_region$REGION_ID), FUN=median)
+
+proj_doc_count_lit <- pals_doc_ea %>%
+  count(Project_Num, `LITIGATED?`)
+proj_doc_count_app <- pals_doc_ea %>%
+  count(Project_Num, `APPEALED OR OBJECTED?`)
+
+aggregate(proj_doc_count_lit$n, list(proj_doc_count_lit$`LITIGATED?`), FUN=mean)
+aggregate(proj_doc_count_lit$n, list(proj_doc_count_lit$`LITIGATED?`), FUN=median)
+
+aggregate(proj_doc_count_app$n, list(proj_doc_count_app$`APPEALED OR OBJECTED?`), FUN=mean)
+aggregate(proj_doc_count_app$n, list(proj_doc_count_app$`APPEALED OR OBJECTED?`), FUN=median)
+
+ggplot(data = aggregate(proj_doc_count_region$n, list(proj_doc_count_region$REGION_ID), FUN=mean), aes(x=Group.1, y=x)) +
+  geom_bar(stat="identity") + 
+  ggtitle("Average Number of Documents per Project by NEPA Stage") + 
+  xlab("FS Region") + ylab("Ave. Number of Documents by Region")
+
+ggplot(data = aggregate(proj_doc_count_region$n, list(proj_doc_count_region$REGION_ID), FUN=median), aes(x=Group.1, y=x)) +
+  geom_bar(stat="identity") + 
+  ggtitle("Average Number of Documents per Project by NEPA Stage") + 
+  xlab("FS Region") + ylab("Medain Number of Documents by Region")
+
+ggplot(data = aggregate(proj_doc_count_lit$n, list(proj_doc_count_lit$`LITIGATED?`), FUN=mean), aes(x=Group.1, y=x)) +
+  geom_bar(stat="identity") + 
+  ggtitle("Average Number of Documents per Project by NEPA Stage") + 
+  xlab("Litigated? (1 = yes, 0 = no") + ylab("Ave. Number of Documents by Litigated?")
+
+ggplot(data = aggregate(proj_doc_count_lit$n, list(proj_doc_count_lit$`LITIGATED?`), FUN=median), aes(x=Group.1, y=x)) +
+  geom_bar(stat="identity") + 
+  ggtitle("Average Number of Documents per Project by NEPA Stage") + 
+  xlab("Litigated? (1 = yes, 0 = no") + ylab("Medain Number of Documents by Litigated?")
+
+ggplot(data = aggregate(proj_doc_count_app$n, list(proj_doc_count_app$`APPEALED OR OBJECTED?`), FUN=mean), aes(x=Group.1, y=x)) +
+  geom_bar(stat="identity") + 
+  ggtitle("Average Number of Documents per Project by NEPA Stage") + 
+  xlab("Appealed? (1 = yes, 0 = no") + ylab("Ave. Number of Documents by Litigated?")
+
+ggplot(data = aggregate(proj_doc_count_app$n, list(proj_doc_count_app$`APPEALED OR OBJECTED?`), FUN=median), aes(x=Group.1, y=x)) +
+  geom_bar(stat="identity") + 
+  ggtitle("Average Number of Documents per Project by NEPA Stage") + 
+  xlab("Appealed? (1 = yes, 0 = no") + ylab("Medain Number of Documents by Appealed?")
+
+# Number of documents through time? Look at both Initiation FY and Signed FY
+# Group by year. Want mean or median documents per project by year? 
+
+proj_doc_count_init <- pals_doc_ea %>%
+  count(Project_Num, `INITIATION FY`)
+  
+proj_doc_count_sign <- pals_doc_ea %>%
+  count(Project_Num, `SIGNED FY`)
+
+ggplot(data = aggregate(proj_doc_count_init$n, list(proj_doc_count_init$`INITIATION FY`), FUN=sum), aes(x = Group.1, y = x)) + 
+  geom_line() + 
+  ggtitle("Total Number of Documents by Fiscal Year NEPA Project Initiated") + 
+  xlab("Fiscal Year") + ylab("Total Number of Documents Related to EAs")
+
+ggplot(data = aggregate(proj_doc_count_sign$n, list(proj_doc_count_sign$`SIGNED FY`), FUN=sum), aes(x = Group.1, y = x)) + 
+  geom_line() + 
+  ggtitle("Total Number of Documents by Fiscal Year NEPA Project Signed") + 
+  xlab("Fiscal Year") + ylab("Total Number of Documents Related to EAs")
+
+# Relationship between number of documents and elapsed days?
+# Total number of documents for a project on x axis, the elapsed days for that project on the y axis.
+# need proj_doc_count and elapsed days for each project (from pals)
+
+proj_doc_elapsed <- left_join(proj_doc_count, pals, by=c('Project_Num'='PROJECT NUMBER')) %>%
+  select(Project_Num, n, `ELAPSED DAYS`)
+
+proj_doc_elapsed %>%
+  ggplot(aes(x = n, y = `ELAPSED DAYS`)) + 
+  geom_point() + 
+  scale_y_continuous(trans='log10') +
+  scale_x_continuous(trans='log10') +
+  ggtitle("Elapsed Days by Number of NEPA Documents in Project") + 
+  xlab("Number of Documents") + ylab("Days to Complete NEPA Analysis")
 
